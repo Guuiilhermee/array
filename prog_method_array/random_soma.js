156765c052212454f54a325ab97c541e072127a1
Guilherme Ambrosio <guiilherme.ambrosio@gmail.com>
2026-02-18 15:49:39 -0300

    soma números aleatórios

---
+++ prog_method_array/random_soma.js
@@ -0,0 +1,24 @@
+// geração de números aleatórios entre min e max
+// equação: Math.random() * (max - min) + min
+// arredondar para cima Math.floor
+
+// programa para gerar 5 números aleatórios e somar todos eles
+
+function gerarNum(min,max){
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function principal(){
+    let num = [], min = 1, max = 10, val = 0, soma = 0
+
+    for(let i = 0; i < 5; i++){
+        val = gerarNum(min,max)
+        num.push(val)
+    }
+    console.log(num)
+    for(let i = 0; i < num.length; i++){
+        soma += num[i]
+    }
+    console.log(` A soma dos valores do array é: ${soma}`)
+}
+principal()
