3eb0dfa151c7b7edd3ee5a39365ffe39f5dcce6e
Guilherme Ambrosio <guiilherme.ambrosio@gmail.com>
2026-02-18 15:39:49 -0300

    random

--- prog_method_array/random.js
+++ prog_method_array/random.js
@@ -1,17 +1,19 @@
 // geração de números aleatórios entre min e max
 // equação: Math.random() * (max - min) + min
+// arredondar para cima Math.floor
 
 // programa para gerar 5 números aleatórios
 
 function gerarNum(min,max){
-    return Math.random() * (max - min) + min
+    return Math.floor(Math.random() * (max - min) + min)
 }
 
 function principal(){
-    let num = [], min = 1, max = 10
+    let num = [], min = 1, max = 10, val = 0
 
     for(let i = 0; i < 5; i++){
-        num.push(i)
+        val = gerarNum(min,max)
+        num.push(val)
     }
     console.log(num)
 }
