54295df8b4b2631e6d8646231866edf28a9394a7
Guilherme Ambrosio <guiilherme.ambrosio@gmail.com>
2026-02-18 16:22:43 -0300

    teste

---
+++ prog_method_array/teste.js
@@ -0,0 +1,30 @@
+// geração de números aleatórios entre min e max
+// equação: Math.random() * (max - min) + min
+// arredondar para cima Math.floor
+
+// programa para gerar 5 números aleatórios, somar todos eles e fazer a média
+
+function gerarNum(min,max){
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function principal(){
+    let num = [], min = 1, max = 10, val = 0, soma = 0, media = 0
+
+    for(let i = 0; i < 7; i++){
+        val = gerarNum(min,max)
+        num.push(val)
+    }
+    console.log(num.sort())
+    console.log(num.length)
+    console.log('----------')
+    for(let i = 0; i < num.length; i++){
+        soma += num[i]
+    }
+    console.log(num.reverse())
+    media = soma / num.length
+    console.log(` A soma dos valores do array é: ${soma}`)
+    console.log(` A media dos valores do array é: ${media.toFixed(3)}`)
+}
+principal()
+
